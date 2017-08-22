e326a85c00ac479cf95074a58ef33467cf5be934
Danny Nhoisaykham <danny@MBP.local>
2017-08-22 18:20:04 -0500

    removed keys

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'JfokwBJkeQCJl622numpLKdxJ',
-  consumer_secret: 'pveSTRCEGsaXlxODoJqtEJrDFi1yMRYVzjlb9kFVqXfLCXtHfw',
-  access_token_key: '33053416-39qzr7YwS7u0fCD6PApXJeuX4gjNQ6NFRTMyXZafE',
-  access_token_secret: 'uvgKVqDPW2oIYi7TaHA1r4Y1a7yAi40ccPHvx2J7SoVJB',
-}
